7cb0ab041ed7b12dc9f87d56a5e81da6692e396b
Andy Heaton <heatonandy@gmail.com>
2013-04-12 23:57:59 -0600

    lots of gui/js work on login and sign up

--- js/prioritize.js
+++ js/prioritize.js
@@ -7,7 +7,7 @@ var wo = (function packageTodo() {
 					 "1QV6cFHk4MsExOzENqSvPKCCyXTz6ldPL268XXF6");
 
 	/* CLASS DECLARATIONS */
-	var ToDo = Parse.Object.extend("ToDo"
+	/*var ToDo = Parse.Object.extend("ToDo"
 		// instance methods
 		{
 			finishTask: function() {
@@ -21,13 +21,102 @@ var wo = (function packageTodo() {
 					} // end error
 				}); // end save
 			} // end finishTask function
-		}); // end ToDo Class
+		}); // end ToDo Class*/
 
 
 	// variable declarations
-
+	var currentUser, tempUser, uName, pass;
 	// function declarations
 	$(function () {
-		
-	}); // end anonymous jquery dialog signin funciton
-}); // end wo 
+		$("#signinPopup").dialog({
+			autoOpen: false,
+			show: {
+				effect: "drop",
+				duration: 500
+			},
+			hide: {
+				effect: "drop",
+				duration: "500"
+			}, 
+			modal: true,
+			width: 315,
+			position: {
+				my: 'top',
+				at: 'right bottom',
+				of: $('#signinDiv')
+			},
+			buttons: [
+				{
+					text: "Sign In",
+					click: function() {
+						uName = $("#usernameInput").val().toString();
+						pass = $("#passwordInput").val().toString();
+						Parse.User.logIn(uName, pass, {
+							success: function(user) {
+								$("#signinPopup").dialog("close");
+							}
+						});
+					} // end click function 
+				}, // end button 1
+				{
+					text: "Close",
+					click: function() {
+						$(this).dialog("close");
+					}
+				}
+			]
+		}); // end signinPopup dialog select
+		$(".ui-dialog-titlebar").hide();
+		// signin/register button click
+		$("#signinBtn").click(function() {
+			$("#signinPopup").dialog("open"); 
+		}); // end #signingBtn select
+		$("#registerPopup").dialog({
+			autoOpen: false,
+			show: {
+				effect: "drop",
+				duration: 500
+			},
+			hide: {
+				effect: "drop",
+				duration: "500"
+			}, 
+			modal: true,
+			width: 315,
+			height: 300,
+			position: {
+				my: 'top',
+				at: 'right bottom',
+				of: $('#signinDiv')
+			},
+			buttons: [
+				{
+					text: "Finish",
+					click: function() {
+						$(this).dialog("close");
+						tempUser = new Parse.User();
+						user.set("firstname", $("#newFNameInput").val().toString());
+						alert("first name: " + $("#newFNameInput").val().toString());
+						user.set("lastname", $("#newLNameInput").val().toString());
+						user.set("username", $("#newUnameInput").val().toString());
+						user.set("password", $("#newPassInput").val().toString());
+
+						user.signUp(null, {
+							success: function(user) {
+								$("#registerPopup").dialog("close");
+							},
+							error: function(user, error) {
+								alert("There was an error: " + error.code + " " + error.message);
+							}
+						}); // end user signup
+					}
+				}
+			]
+		}); // end signinPopup dialog select
+		$(".ui-dialog-titlebar").hide();
+		$("#registerBtn").click(function() {
+			$("#signinPopup").dialog("close"); 
+			$("#registerPopup").dialog("open"); 
+		}); // end #signingBtn select
+	}); // end anonymous jquery dialog funciton
+}()); // end wo 
