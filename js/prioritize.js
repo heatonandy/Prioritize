887c983c3ab9dbd6de8a91bec27399005d5483a7
Andy Heaton <heatonandy@gmail.com>
2013-04-16 19:52:29 -0600

    more ui work on todoDiv including new check mark!

--- js/prioritize.js
+++ js/prioritize.js
@@ -7,21 +7,22 @@ var wo = (function packageTodo() {
 					 "1QV6cFHk4MsExOzENqSvPKCCyXTz6ldPL268XXF6");
 
 	/* CLASS DECLARATIONS */
-	/*var ToDo = Parse.Object.extend("ToDo"
+	var ToDo = Parse.Object.extend("ToDo",
 		// instance methods
 		{
 			finishTask: function() {
 				this.set("completed", true);
 				this.save(null, {
-					success: function(this){
-						alert(this.task + " has been marked completed in the database");
+					success: function() {
+						alert("task has been marked completed in the database");
 					}, // end success
-					error: function(this, error) {
+					error: function(error) {
 						alert("Task could not be marked complete in the database");
 					} // end error
 				}); // end save
 			} // end finishTask function
-		}); // end ToDo Class*/
+		}); // end ToDo Class
+
 	var Tag = Parse.Object.extend("Tag");
 
 	// variable declarations
@@ -32,7 +33,7 @@ var wo = (function packageTodo() {
 	
 	// jQuery dialog login functions
 	$(function () {
-		// login div jquery dialog
+		/***JQUERY DIALOG CODE JQUERY DIALOG CODE JQUERY DIALOG CODE JQUERY DIALOG CODE JQUERY DIALOG CODE ***/
 		$("#loginPopup").dialog({
 			autoOpen: false,
 			show: {
@@ -167,6 +168,7 @@ var wo = (function packageTodo() {
 			$(".userBtnClass").val("Login/Register").attr('class', 'loginBtnClass');
 			$("#logoutPopup").dialog("close");
 		});
+		/***JQUERY SORTABLE CODE JQUERY SORTABLE CODE JQUERY SORTABLE CODE JQUERY SORTABLE CODE JQUERY SORTABLE CODE ***/
 		$( "#tagSortable" ).sortable({
       		placeholder: "ui-state-highlight",
       		start: function (e,ui){        // new lines to
@@ -188,6 +190,8 @@ var wo = (function packageTodo() {
    		});
     	$( "#todoSortable" ).disableSelection();
 	}); // end anonymous jquery dialog funciton
+
+	/***FUNCTIONS FOR CALLING IN THE DOM***/
 	return {
 		createNewTag: function () {
 			var tagTitle = $('#tagInput').val().toString();
@@ -201,7 +205,7 @@ var wo = (function packageTodo() {
 		createNewTodo: function () {
 			var todoTitle = $('#todoInput').val().toString();
 			if(todoTitle.trim() !== '') {
-				$('#todoSortable').prepend("<li class='ui-state-default sortableItem'><input class='sortableItem' type='button' value='" + todoTitle + "' /></li><br>");
+				$('#todoSortable').prepend("<li class='ui-state-default sortableItem'><input class='checkImg' type='image' src='img/whitecheckmark.png' /><input class='sortableItem' type='button' value='" + todoTitle + "' /></li><br>");
 				$('#todoInput').val('');
 			}
 		}
